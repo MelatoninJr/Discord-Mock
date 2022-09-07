7c28043c400e5bb0e8639aa62340e97ce827cb2d
Dillon M <dillon32300@gmail.com>
2022-09-07 14:58:58 -0500

    saving

--- discord/src/components/rightsidebar.js
+++ discord/src/components/rightsidebar.js
@@ -4,6 +4,12 @@ import { RiInboxFill, RiQuestionFill, RiSearchLine } from 'react-icons/ri'
 import Cloud from './assets/cloud.JPG'
 import Online from './assets/online.png'
 import Person from './assets/person.png'
+import Cat from './assets/cat.png'
+import Crab from './assets/crab.png'
+import Frog from './assets/frog.png'
+import Cake from './assets/cake.png'
+
+
 
 const RightSideBar = () => {
     return (
@@ -17,7 +23,7 @@ const RightSideBar = () => {
             </div>
             <div className="column-container-two-right">
                 <div className="rsb-inner-container-one">
-                    <div className="rsb-roll-title">HELPER - 2</div>
+                    <div className="rsb-roll-title">HELPER - 1</div>
                     <div className="rsb-roll-container">
                         <div className="rsb-individual-roll">
                             <div className="rsb-roll-pfp">
@@ -28,6 +34,8 @@ const RightSideBar = () => {
                                 MelatoninJr
                             </div>
                         </div>
+
+                        <div className="rsb-roll-title-two">FRIENDS - 5</div>
                         <div className="rsb-individual-roll">
                             <div className="rsb-roll-pfp">
                                 <img src={Person} className='cloud-pfp'></img>
@@ -36,6 +44,47 @@ const RightSideBar = () => {
                             <div className="rsb-roll-name">
                                 Shorty
                             </div>
+                            
+                        </div>
+                        <div className="rsb-individual-roll">
+                            <div className="rsb-roll-pfp">
+                                <img src={Cat} className='cloud-pfp'></img>
+                                <img src={Online} className='online-pfp'></img>
+                            </div>
+                            <div className="rsb-roll-name">
+                                SlothCat
+                            </div>
+                            
+                        </div>
+                        <div className="rsb-individual-roll">
+                            <div className="rsb-roll-pfp">
+                                <img src={Crab} className='cloud-pfp'></img>
+                                <img src={Online} className='online-pfp'></img>
+                            </div>
+                            <div className="rsb-roll-name">
+                                HaroldZoid
+                            </div>
+                            
+                        </div>
+                        <div className="rsb-individual-roll">
+                            <div className="rsb-roll-pfp">
+                                <img src={Frog} className='cloud-pfp'></img>
+                                <img src={Online} className='online-pfp'></img>
+                            </div>
+                            <div className="rsb-roll-name">
+                                Lilly
+                            </div>
+                            
+                        </div>
+                        <div className="rsb-individual-roll">
+                            <div className="rsb-roll-pfp">
+                                <img src={Cake} className='cloud-pfp'></img>
+                                <img src={Online} className='online-pfp'></img>
+                            </div>
+                            <div className="rsb-roll-name">
+                                Cake
+                            </div>
+                            
                         </div>
 
                     </div>
